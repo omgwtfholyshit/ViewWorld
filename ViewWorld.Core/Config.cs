259a8b7e0d0c6d8246c284042b1396ea6dcc72be
Jeremy <liuzhen122087@msn.com>
2016-07-23 09:55:58 +0800

    添加了pagedlist的引用以及创建了config文件

---
+++ ViewWorld.Core/Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Configuration;
+
+namespace Website.Core
+{
+    public class Config
+    {
+        public static string host_name { get; private set; }
+        public static int db_port { get; private set; }
+
+        static Config()
+        {
+            host_name = ConfigurationManager.AppSettings["dbHostName"];
+            db_port = Convert.ToInt32(ConfigurationManager.AppSettings["dbPort"]);
+        }
+    }
+}
--- ViewWorld/Controllers/BaseController.cs
+++ ViewWorld/Controllers/BaseController.cs
@@ -1,17 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Microsoft.AspNet.Identity;
+using PagedList;
+using System;
+using System.Text;
 using System.Web.Mvc;
+using ViewWorld.Core;
 
 namespace ViewWorld.Controllers
 {
     public class BaseController : Controller
     {
-        // GET: Base
-        public ActionResult Index()
+        protected override JsonResult Json(object data, string contentType,
+           Encoding contentEncoding, JsonRequestBehavior behavior)
         {
-            return View();
+            return new JsonNetResult
+            {
+                Data = data,
+                ContentType = contentType,
+                ContentEncoding = contentEncoding,
+                JsonRequestBehavior = behavior
+            };
+        }
+
+        protected new JsonResult Json(Object data)
+        {
+            var data2 = new
+            {
+                status = 200,
+                message = "",
+                data = data
+            };
+            return Json(data2, JsonRequestBehavior.AllowGet);
+        }
+        protected JsonResult Json(string status, Object data)
+        {
+            var data2 = new
+            {
+                status = status,
+                message = "",
+                data = data
+            };
+            return Json(data2, JsonRequestBehavior.AllowGet);
+        }
+
+        protected JsonResult PageJson(IPagedList data)
+        {
+            var pageData = new
+            {
+                PageCount = data.PageCount,
+                PageIndex = data.PageNumber,
+                PageSize = data.PageSize,
+                Data = data
+            };
+            return Json(pageData);
+        }
+
+        protected JsonResult SuccessJson()
+        {
+            var result = new
+            {
+                status = "200"
+            };
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        protected JsonResult ErrorJson(string msg)
+        {
+            var result = new
+            {
+                status = "300",
+                message = msg
+            };
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+        protected JsonResult ErrorJson(string status, string msg)
+        {
+            var result = new
+            {
+                status = status,
+                message = msg
+            };
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        protected string UserId
+        {
+            get
+            {
+                return User.Identity.GetUserId();
+            }
         }
     }
 }
