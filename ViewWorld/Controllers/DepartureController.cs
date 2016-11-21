482a4199b296d8ecd876a0d61084d3fd9ea70871
Kevin <Kevin@DESKTOP-GSSSQKT>
2016-11-21 16:41:00 +1100

    add departure wip
    - work in progress

---
+++ ViewWorld/Controllers/DepartureController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewWorld.Models;
+using ViewWorld.Models.Trip;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
+namespace ViewWorld.Controllers
+{
+    public class DepartureController : BaseController
+    {
+        MongoRepository repo;
+        public DepartureController()
+            :this (new MongoRepository())
+        {
+
+        }
+        public DepartureController(MongoRepository _repo)
+        {
+            repo = _repo;
+        }
+        public async Task<ActionResult> AddDeparture(StartingPoint data)
+        {
+            data.UpdatedBy = User.Identity.Name;
+            var result = await repo.AddOne<StartingPoint>(data);
+            if (result.Success)
+            {
+                return SuccessJson();
+            }
+            else
+            {
+                return ErrorJson(result.Message);
+            }
+        }
+    }
+}
--- ViewWorld/Scripts/Backend/Trip/DepartureManagement.js
+++ ViewWorld/Scripts/Backend/Trip/DepartureManagement.js
@@ -4,19 +4,14 @@
             $('.ui.modal.add-departure-modal').modal('show');
             $('.ui.dropdown.provider-list').dropdown({
                 apiSettings: {
-                    mockResponse: {
-                        success: true,
-                        results: [
-                            { name: "test", value: 1 },
-                            { name: "test2", value: 2 }
-                        ]
-                    }
-                }
+                    url: '/Provider/GetAll',
+                    saveRemoteData: false
+                },
             });
         });
         $('.button#submit-form').on('click', function (e) {
             e.preventDefault();
-            submitForm(e);
+            $('.ui.form#add-departure').form('validate form');
         });
         $('.button.edit').on('click', function (e) {
             e.preventDefault();
@@ -35,11 +30,74 @@
             if (r) {
                 saveDeparture(e);
             }
+        });        
+        
+    }
+
+    function initFormValidator() {
+        $('.ui.form#add-departure').form({
+            inline: true,
+            on    : 'blur',
+            fields: {
+                landmark: {
+                    identifier: 'landmark',
+                    rules: [
+                        {
+                            type: 'empty',
+                            prompt: '地点不能为空'
+                        }
+                    ]
+                },
+                address: {
+                    identifier: 'address',
+                    rules: [
+                        {
+                            type: 'empty',
+                            prompt: '地址不能为空'
+                        }
+                    ]
+                },
+                depart_time: {
+                    identifier: 'depart_time',
+                    rules: [
+                        {
+                            type: 'empty',
+                            prompt: '出发时间不能为空'
+                        }
+                    ]
+                },
+                provider: {
+                    identifier: 'provider',
+                    rules: [
+                        {
+                            tyep: 'empty',
+                            prompt: '供应商不能为空'
+                        }
+                    ]
+                }
+            },
+            onSuccess: function (e, fields) {
+                return submitForm($(this));
+            },
+            onFailure: function (errors, fields) {
+                console.log(errors);
+            }
         });
     }
-    
 
+    function submitForm($form) {
+        var $submitButton = $form.find('#submit-form');
+        if ($submitButton.hasClass('loading')) {
+            return;
+        }
+        $submitButton.addClass('loading');
+        var $inputs = $form.find('input');
+        var data = {};
+
+    }
+    
     function initPage() {
+        initFormValidator();
         bindEvents();
     }
 
